aa7a3210f784bf28a2502f6a21f73c3f163ceece
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2024-08-30 15:40:22 +0200

    Improving Flax intro.

--- train.py
+++ train.py
@@ -1,166 +1,155 @@
-# Copyright 2024 The Flax Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""MNIST example.
-
-Library file which executes the training and evaluation loop for MNIST.
-The data is loaded using tensorflow_datasets.
-"""
-
-# See issue #620.
-# pytype: disable=wrong-keyword-args
-
 from absl import logging
-from flax import linen as nn
-from flax.metrics import tensorboard
-from flax.training import train_state
-import jax
-import jax.numpy as jnp
-import ml_collections
+
+# from flax import linen as nn
+from flax import nnx
+
+# from flax.metrics import tensorboard
+# from flax.training import train_state
+# import jax
+
+from jax import random
+
+# import jax.numpy as jnp
+# import ml_collections
 import numpy as np
-import optax
-import tensorflow_datasets as tfds
+
+# import optax
+from tqdm.notebook import tqdm  # progress bar
 
 
-class CNN(nn.Module):
-  """A simple CNN model."""
+# Train for a single epoch
+def _train_epoch(
+    model, loss_fn, optimizer, xs_train, ys_train, xs_test, ys_test, batch_size, rng
+):
+    train_ds_size = len(xs_train)
+    steps_per_epoch = train_ds_size // batch_size
 
-  @nn.compact
-  def __call__(self, x):
-    x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-    x = nn.relu(x)
-    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-    x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-    x = nn.relu(x)
-    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-    x = x.reshape((x.shape[0], -1))  # flatten
-    x = nn.Dense(features=256)(x)
-    x = nn.relu(x)
-    x = nn.Dense(features=10)(x)
-    return x
+    def train_step(model, optimizer, loss_fn, x, y):
+        loss, grads = nnx.value_and_grad(loss_fn)(
+            model, x, y
+        )  # get loss and gradients using JAX autodiff
+        # update parameters of the model according to the optimizer
+        optimizer.update(grads)
+        return loss
 
+    perms = random.permutation(
+        rng, len(xs_train)
+    )  # shuffle dataset (permutation of samples order)
+    perms = perms[: steps_per_epoch * batch_size]  # skip incomplete batch
+    perms = perms.reshape((steps_per_epoch, batch_size))
 
-@jax.jit
-def apply_model(state, images, labels):
-  """Computes gradients, loss and accuracy for a single batch."""
+    epoch_loss = []
 
-  def loss_fn(params):
-    logits = state.apply_fn({'params': params}, images)
-    one_hot = jax.nn.one_hot(labels, 10)
-    loss = jnp.mean(optax.softmax_cross_entropy(logits=logits, labels=one_hot))
-    return loss, logits
+    for perm in perms:
+        batch_xs = xs_train[perm, :]
+        batch_ys = ys_train[perm, :]
+        loss = train_step(model, optimizer, loss_fn, batch_xs, batch_ys)
+        epoch_loss.append(loss)  # store training loss for the current batch
 
-  grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-  (loss, logits), grads = grad_fn(state.params)
-  accuracy = jnp.mean(jnp.argmax(logits, -1) == labels)
-  return grads, loss, accuracy
-
-
-@jax.jit
-def update_model(state, grads):
-  return state.apply_gradients(grads=grads)
+    train_loss = np.mean(epoch_loss)
+    test_loss = loss_fn(model, xs_test, ys_test)
+    return model, train_loss, test_loss
 
 
-def train_epoch(state, train_ds, batch_size, rng):
-  """Train for a single epoch."""
-  train_ds_size = len(train_ds['image'])
-  steps_per_epoch = train_ds_size // batch_size
+def train(
+    model,
+    optimizer,
+    loss_fn,
+    xs_train,
+    xs_test,
+    ys_train,
+    ys_test,
+    batch_size,
+    epochs,
+    log_period_epoch=1,
+    show_progress=True,
+):
 
-  perms = jax.random.permutation(rng, len(train_ds['image']))
-  perms = perms[: steps_per_epoch * batch_size]  # skip incomplete batch
-  perms = perms.reshape((steps_per_epoch, batch_size))
+    train_loss_history = []
+    test_loss_history = []
 
-  epoch_loss = []
-  epoch_accuracy = []
-
-  for perm in perms:
-    batch_images = train_ds['image'][perm, ...]
-    batch_labels = train_ds['label'][perm, ...]
-    grads, loss, accuracy = apply_model(state, batch_images, batch_labels)
-    state = update_model(state, grads)
-    epoch_loss.append(loss)
-    epoch_accuracy.append(accuracy)
-  train_loss = np.mean(epoch_loss)
-  train_accuracy = np.mean(epoch_accuracy)
-  return state, train_loss, train_accuracy
-
-
-def get_datasets():
-  """Load MNIST train and test datasets into memory."""
-  ds_builder = tfds.builder('mnist')
-  ds_builder.download_and_prepare()
-  train_ds = tfds.as_numpy(ds_builder.as_dataset(split='train', batch_size=-1))
-  test_ds = tfds.as_numpy(ds_builder.as_dataset(split='test', batch_size=-1))
-  train_ds['image'] = jnp.float32(train_ds['image']) / 255.0
-  test_ds['image'] = jnp.float32(test_ds['image']) / 255.0
-  return train_ds, test_ds
-
-
-def create_train_state(rng, config):
-  """Creates initial `TrainState`."""
-  cnn = CNN()
-  params = cnn.init(rng, jnp.ones([1, 28, 28, 1]))['params']
-  tx = optax.sgd(config.learning_rate, config.momentum)
-  return train_state.TrainState.create(apply_fn=cnn.apply, params=params, tx=tx)
-
-
-def train_and_evaluate(
-    config: ml_collections.ConfigDict, workdir: str
-) -> train_state.TrainState:
-  """Execute model training and evaluation loop.
-
-  Args:
-    config: Hyperparameter configuration for training and evaluation.
-    workdir: Directory where the tensorboard summaries are written to.
-
-  Returns:
-    The train state (which includes the `.params`).
-  """
-  train_ds, test_ds = get_datasets()
-  rng = jax.random.key(0)
-
-  summary_writer = tensorboard.SummaryWriter(workdir)
-  summary_writer.hparams(dict(config))
-
-  rng, init_rng = jax.random.split(rng)
-  state = create_train_state(init_rng, config)
-
-  for epoch in range(1, config.num_epochs + 1):
-    rng, input_rng = jax.random.split(rng)
-    state, train_loss, train_accuracy = train_epoch(
-        state, train_ds, config.batch_size, input_rng
-    )
-    _, test_loss, test_accuracy = apply_model(
-        state, test_ds['image'], test_ds['label']
-    )
-
-    logging.info(
-        'epoch:% 3d, train_loss: %.4f, train_accuracy: %.2f, test_loss: %.4f,'
-        ' test_accuracy: %.2f'
-        % (
-            epoch,
-            train_loss,
-            train_accuracy * 100,
-            test_loss,
-            test_accuracy * 100,
+    for epoch in tqdm(range(1, epochs + 1), disable=not show_progress):
+        model, train_loss, test_loss = _train_epoch(
+            model,
+            loss_fn,
+            optimizer,
+            xs_train,
+            ys_train,
+            xs_test,
+            ys_test,
+            batch_size,
+            random.key(1),
         )
-    )
 
-    summary_writer.scalar('train_loss', train_loss, epoch)
-    summary_writer.scalar('train_accuracy', train_accuracy, epoch)
-    summary_writer.scalar('test_loss', test_loss, epoch)
-    summary_writer.scalar('test_accuracy', test_accuracy, epoch)
+        train_loss_history.append(train_loss)
+        test_loss_history.append(test_loss)
 
-  summary_writer.flush()
-  return state
+        if epoch == 1 or epoch % log_period_epoch == 0:
+            logging.info(
+                "epoch:% 3d, train_loss: %.4f, test_loss: %.4f,"
+                % (
+                    epoch,
+                    train_loss,
+                    test_loss,
+                )
+            )
+    return train_loss_history, test_loss_history
+
+
+# def create_train_state(rng, config):
+#     """Creates initial `TrainState`."""
+#     cnn = CNN()
+#     params = cnn.init(rng, jnp.ones([1, 28, 28, 1]))["params"]
+#     tx = optax.sgd(config.learning_rate, config.momentum)
+#     return train_state.TrainState.create(apply_fn=cnn.apply, params=params, tx=tx)
+
+
+# def train_and_evaluate(
+#     config: ml_collections.ConfigDict, workdir: str
+# ) -> train_state.TrainState:
+#     """Execute model training and evaluation loop.
+
+#     Args:
+#       config: Hyperparameter configuration for training and evaluation.
+#       workdir: Directory where the tensorboard summaries are written to.
+
+#     Returns:
+#       The train state (which includes the `.params`).
+#     """
+#     train_ds, test_ds = get_datasets()
+#     rng = jax.random.key(0)
+
+#     summary_writer = tensorboard.SummaryWriter(workdir)
+#     summary_writer.hparams(dict(config))
+
+#     rng, init_rng = jax.random.split(rng)
+#     state = create_train_state(init_rng, config)
+
+#     for epoch in range(1, config.num_epochs + 1):
+#         rng, input_rng = jax.random.split(rng)
+#         state, train_loss, train_accuracy = train_epoch(
+#             state, train_ds, config.batch_size, input_rng
+#         )
+#         _, test_loss, test_accuracy = apply_model(
+#             state, test_ds["image"], test_ds["label"]
+#         )
+
+#         logging.info(
+#             "epoch:% 3d, train_loss: %.4f, train_accuracy: %.2f, test_loss: %.4f,"
+#             " test_accuracy: %.2f"
+#             % (
+#                 epoch,
+#                 train_loss,
+#                 train_accuracy * 100,
+#                 test_loss,
+#                 test_accuracy * 100,
+#             )
+#         )
+
+#         summary_writer.scalar("train_loss", train_loss, epoch)
+#         summary_writer.scalar("train_accuracy", train_accuracy, epoch)
+#         summary_writer.scalar("test_loss", test_loss, epoch)
+#         summary_writer.scalar("test_accuracy", test_accuracy, epoch)
+
+#     summary_writer.flush()
+#     return state
